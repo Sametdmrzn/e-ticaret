7f9e3b6cbf389317d747da301ed0cfc2971fab05
Samet DEMİREZEN <73247768+Sametdmrzn@users.noreply.github.com>
2025-01-29 20:36:38 +0300

    added sub-menu

--- Son-Deneme/script.js
+++ Son-Deneme/script.js
@@ -5,3 +5,22 @@ const mobileMenu = document.querySelector('#mobileMenu');
 navbarToggler.addEventListener('click', () => {
     mobileMenu.classList.toggle('open');
 });
+
+
+// Dropdown menüyü açma / kapama
+const dropdownLinks = document.querySelectorAll('.mobile-menu > ul > li > a');
+
+dropdownLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        const submenu = link.nextElementSibling; // Alt menüyü al
+        const arrow = link.querySelector('.toggle-arrow'); // Ok işaretini al
+        
+        if (submenu && submenu.classList.contains('sub-menu')) {
+            e.preventDefault(); // Menü linkine gitmesini engelle
+            submenu.classList.toggle('open'); // Alt menüyü aç / kapa
+            arrow.classList.toggle('open'); // Ok işaretini döndür
+        }
+    });
+});
+
+
