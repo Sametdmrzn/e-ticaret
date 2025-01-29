1298cf2f594ebe8feca6bd1cfaf4792f3aea46cb
Samet DEMİREZEN <73247768+Sametdmrzn@users.noreply.github.com>
2025-01-30 00:45:30 +0300

    bugs fixed

--- Son-Deneme/script.js
+++ Son-Deneme/script.js
@@ -1,11 +1,11 @@
 const navbarToggler = document.querySelector('.navbar-toggler');
 const mobileMenu = document.querySelector('#mobileMenu');
 
+
 // Hamburger menüsüne tıklanınca mobil menüyü aç/kapat
 navbarToggler.addEventListener('click', () => {
     mobileMenu.classList.toggle('open');
 });
-
 
 // Dropdown menüyü açma / kapama
 const dropdownLinks = document.querySelectorAll('.mobile-menu > ul > li > a');
@@ -14,11 +14,23 @@ dropdownLinks.forEach(link => {
     link.addEventListener('click', (e) => {
         const submenu = link.nextElementSibling; // Alt menüyü al
         const arrow = link.querySelector('.toggle-arrow'); // Ok işaretini al
-        
+
         if (submenu && submenu.classList.contains('sub-menu')) {
             e.preventDefault(); // Menü linkine gitmesini engelle
-            submenu.classList.toggle('open'); // Alt menüyü aç / kapa
-            arrow.classList.toggle('open'); // Ok işaretini döndür
+
+            // Önce diğer açık menüleri kapat (Eğer birden fazla açık olsun istemiyorsan)
+            document.querySelectorAll('.sub-menu.open').forEach(openMenu => {
+                if (openMenu !== submenu) {
+                    openMenu.classList.remove('open');
+                    openMenu.previousElementSibling.querySelector('.toggle-arrow').classList.remove('open');
+                }
+            });
+
+            // Seçili menüyü aç/kapa
+            submenu.classList.toggle('open'); 
+            if (arrow) {
+                arrow.classList.toggle('open'); // Ok işaretini döndür
+            }
         }
     });
 });
